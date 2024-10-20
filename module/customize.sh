ae0d7ed65a8d6b4c833dabf487e5aed984e899a7
Qi_Serein <157876088+Qi-Serein@users.noreply.github.com>
2024-10-20 11:59:17 +0800

    Add files via upload

--- module/customize.sh
+++ module/customize.sh
@@ -1,18 +1,3 @@
-#!/system/bin/sh
-#
-# Copyright 2023 shadow3aaa@gitbub.com
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-#  You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 LOCALE=$(getprop persist.sys.locale)
 
 local_print() {
@@ -24,6 +9,9 @@ local_print() {
 }
 
 if [ "$(getprop fas-rs-installed)" = "" ]; then
-	local_print "请先安装fas-rs再安装此插件" "Please install fas-rs first"
+	local_print "⚠️请先安装fas-rs再安装此插件" "Please install fas-rs first"
 	abort
+elif [ "$(getprop ro.soc.model)" != "SM7475" ]; then
+    local_print "⚠️检测到非目标soc" "Non-Target soc detected"
+    abort
 fi
