64bf6ab05f2e813d023d3879c0024352a0a5c7f9
Qi_Serein <157876088+Qi-Serein@users.noreply.github.com>
2024-10-20 22:18:08 +0800

    Delete module directory

--- module/service.sh
+++
@@ -1,9 +0,0 @@
-MODDIR=${0%/*}
-EXTENSIONS=/dev/fas_rs/extensions
-
-until [ -d $EXTENSIONS ]; do
-	sleep 1s
-done
-
-id=$(awk -F= '/id/ {print $2}' $MODDIR/module.prop)
-cp -f $MODDIR/main.lua $EXTENSIONS/${id}.lua
