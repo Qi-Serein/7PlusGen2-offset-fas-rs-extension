87da60b8aa992eb6c33167a8f93199f9f0aba464
Qi_Serein <157876088+Qi-Serein@users.noreply.github.com>
2024-11-09 10:28:40 +0800

    update v0.3.0

--- main.lua
+++ main.lua
@@ -5,28 +5,31 @@ set_ignore_policy(0, true)
 function load_fas(pid, pkg)
     if (pkg == "com.tencent.tmgp.sgame")
     then
-        set_policy_freq_offset(4, -100000)
-        set_policy_freq_offset(7, -400000)
+        set_policy_freq_offset(4, -150000)
+        set_policy_freq_offset(7, -200000)
+    elseif (pkg == "com.tencent.tmgp.pubgmhd" or pkg == "com.rekoo.pubgm" or pkg == "com.tencent.ig")
+    then
+        set_policy_freq_offset(4, -300000)
     elseif (pkg == "com.miHoYo.Yuanshen" or pkg == "com.miHoyo.ys.bilibili")
     then
-        set_policy_freq_offset(4, 100000)
+        set_policy_freq_offset(4, -100000)
     elseif (pkg == "com.miHoYo.Nap" or pkg == "com.miHoYo.Nap.bilibili")
     then
         set_ignore_policy(4, true)
-    elseif (pkg == "com.tencent.tmgp.pubgmhd")
-    then
-        set_policy_freq_offset(4, -300000)
     elseif (pkg == "com.miHoYo.hkrpg" or pkg == "com.miHoYo.hkrpg.bilibili")
     then
-        set_ignore_policy(4, true)
+        set_ignore_policy(4, -100000)
     elseif (pkg == "com.netease.aceracer")
     then
         set_policy_freq_offset(4, -100000)
         set_policy_freq_offset(7, -300000)
     elseif (pkg == "com.tencent.KiHan")
     then
-        set_policy_freq_offset(4, -500000)
-        elseif (pkg == "com.tencent.tmgp.cod")
+        set_policy_freq_offset(4, -400000)
+    elseif (pkg == "com.tencent.tmgp.cod")
+    then
+        set_policy_freq_offset(4, -400000)
+    elseif (pkg == "com.netease.l22")
     then
         set_policy_freq_offset(4, -400000)
     end
