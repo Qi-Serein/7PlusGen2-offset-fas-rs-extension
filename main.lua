df574f1d810d6a2d0e027abf4fbc22e025283e99
Qi_Serein <157876088+Qi-Serein@users.noreply.github.com>
2024-11-16 11:26:48 +0800

    Update v0.3.0

--- main.lua
+++ main.lua
@@ -1,12 +1,10 @@
 API_VERSION = 3
 
-set_ignore_policy(0, true)
-
 function load_fas(pid, pkg)
     if (pkg == "com.tencent.tmgp.sgame")
     then
+        set_policy_freq_offset(0, -300000)
         set_policy_freq_offset(4, -150000)
-        set_policy_freq_offset(7, -200000)
     elseif (pkg == "com.tencent.tmgp.pubgmhd" or pkg == "com.rekoo.pubgm" or pkg == "com.tencent.ig")
     then
         set_policy_freq_offset(4, -300000)
@@ -15,7 +13,7 @@ function load_fas(pid, pkg)
         set_policy_freq_offset(4, -100000)
     elseif (pkg == "com.miHoYo.Nap" or pkg == "com.miHoYo.Nap.bilibili")
     then
-        set_ignore_policy(4, true)
+        set_ignore_policy(4, -100000)
     elseif (pkg == "com.miHoYo.hkrpg" or pkg == "com.miHoYo.hkrpg.bilibili")
     then
         set_ignore_policy(4, -100000)
@@ -25,12 +23,20 @@ function load_fas(pid, pkg)
         set_policy_freq_offset(7, -300000)
     elseif (pkg == "com.tencent.KiHan")
     then
-        set_policy_freq_offset(4, -400000)
+        set_policy_freq_offset(0, -300000)
+        set_policy_freq_offset(4, -250000)
     elseif (pkg == "com.tencent.tmgp.cod")
     then
-        set_policy_freq_offset(4, -400000)
+        set_policy_freq_offset(4, -300000)
     elseif (pkg == "com.netease.l22")
     then
+        set_policy_freq_offset(4, -350000)
+    elseif (pkg == "com.tencent.tmgp.cf")
+    then
+        set_policy_freq_offset(4, -300000)
+    elseif (pkg == "com.netease.dfjs.mi")
+    then
+        set_policy_freq_offset(0, -200000)
         set_policy_freq_offset(4, -400000)
     end
 end
