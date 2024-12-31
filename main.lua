aa7dc89f78732ecacaa9842e2d0f9fe8294f5e7f
Qi_Serein <157876088+Qi-Serein@users.noreply.github.com>
2024-12-31 20:51:23 +0800

    Update v0.4.0

--- main.lua
+++ main.lua
@@ -1,53 +1,55 @@
-API_VERSION = 3
+API_VERSION = 4
 
 function load_fas(pid, pkg)
     if (pkg == "com.tencent.tmgp.sgame")
     then
-        set_policy_freq_offset(0, -300000)
-        set_policy_freq_offset(4, -150000)
+        set_extra_policy_rel(0, 4, -300000, 0)
+        set_extra_policy_abs(7, 2000000, 2750000)
     elseif (pkg == "com.tencent.tmgp.pubgmhd" or pkg == "com.rekoo.pubgm" or pkg == "com.tencent.ig")
     then
-        set_policy_freq_offset(4, -300000)
+        set_extra_policy_rel(4, 7, -300000, 0)
     elseif (pkg == "com.miHoYo.Yuanshen" or pkg == "com.miHoyo.ys.bilibili")
     then
-        set_policy_freq_offset(4, -100000)
+        set_extra_policy_abs(4, 1800000, 2300000)
     elseif (pkg == "com.miHoYo.Nap" or pkg == "com.miHoYo.Nap.bilibili")
     then
-	set_ignore_policy(0, true)
-        set_policy_freq_offset4, -100000)
+	    set_ignore_policy(0, true)
+        set_extra_policy_abs(4, 2000000, 2350000)
     elseif (pkg == "com.miHoYo.hkrpg" or pkg == "com.miHoYo.hkrpg.bilibili")
     then
-	set_ignore_policy(0, true)
-        set_policy_freq_offset(4, -100000)
+	    set_ignore_policy(0, true)
+        set_extra_policy_abs(4, 2000000, 2350000)
     elseif (pkg == "com.netease.aceracer")
     then
-        set_policy_freq_offset(4, -100000)
-        set_policy_freq_offset(7, -300000)
+        set_extra_policy_rel(4, 7, -300000, -100000)
     elseif (pkg == "com.tencent.KiHan")
     then
-        set_policy_freq_offset(0, -300000)
-        set_policy_freq_offset(4, -250000)
+        set_extra_policy_rel(0, 4, -300000, 0)
     elseif (pkg == "com.tencent.tmgp.cod")
     then
-        set_policy_freq_offset(4, -300000)
+        set_extra_policy_rel(0, 4, -300000, 0)
     elseif (pkg == "com.netease.l22")
     then
-        set_policy_freq_offset(4, -350000)
+        set_extra_policy_rel(0, 4, -350000, 0)
     elseif (pkg == "com.tencent.tmgp.cf")
     then
-        set_policy_freq_offset(4, -300000)
+        set_extra_policy_rel(0, 4, -300000, 0)
     elseif (pkg == "com.netease.dfjs.mi")
     then
-        set_policy_freq_offset(0, -200000)
-        set_policy_freq_offset(4, -400000)
+        set_extra_policy_rel(0, 4, -300000, 0)
     elseif (pkg == "com.tencent.lolm")
     then
-        set_policy_freq_offset(0, -200000)
-        set_policy_freq_offset(4, -200000)
+        set_extra_policy_rel(0, 4, -300000, -100000)
+    elseif (pkg == "com.kurogame.mingchao")
+    then
+        set_extra_policy_rel(0, 4, -300000, -100000)
+        set_extra_policy_abs(7, 2300000, 2800000)
     end
 end
 
 function unload_fas()
     set_ignore_policy(4, false)
-    set_policy_freq_offset(4, 0)
+    remove_extra_policy(0)
+    remove_extra_policy(4)
+    remove_extra_policy(7)
 end
